09987d00859ab03cceabd82d4e7a513eba2bf739
Xavier Mignot <xavier.mignot@floa.com>
2021-10-28 11:11:35 +0200

    Upgrade tf providers

--- eng/tf/tf-providers.tf
+++ eng/tf/tf-providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.74.0"
+      version = "2.82.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.6.0"
+      version = "2.7.0"
     }
   }
 }
