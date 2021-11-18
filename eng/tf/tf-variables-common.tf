f3c38127c2d160f9d1795cacee3982a977e317a2
Xavier Mignot <xavier.mignot@floa.com>
2021-11-18 15:39:14 +0100

    Split variables between specifics and commons

---
+++ eng/tf/tf-variables-common.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  type        = string
+  description = "the location to use for all resources"
+}
+
+variable "project" {
+  type        = string
+  description = "the project name to use in all resource names"
+}
