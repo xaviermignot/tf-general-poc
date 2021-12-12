bb283fd27053159b14d3d63c292ebbb832740a17
Xavier Mignot <code@xmi.fr>
2021-12-12 23:44:38 +0100

    dns progress

--- eng/tf/az-dns.tf
+++ eng/tf/az-dns.tf
@@ -3,16 +3,32 @@ data "azurerm_dns_zone" "dns" {
   resource_group_name = var.dns_zone_rg_name
 }
 
-resource "azurerm_dns_a_record" "app_gw_app" {
-  name                = "appgw-app"
+locals {
+  app_service_subdomain = "appgw-app"
+}
+
+# DNS records for app service
+# A record to link subdomain to app gateway
+# resource "azurerm_dns_a_record" "app_gw_app" {
+#   name                = local.app_service_subdomain
+#   zone_name           = data.azurerm_dns_zone.dns.name
+#   resource_group_name = data.azurerm_dns_zone.dns.resource_group_name
+#   ttl                 = 300
+#   target_resource_id  = azurerm_public_ip.appgw.id
+# }
+
+# CNAME record to link subdomain to app service
+resource "azurerm_dns_cname_record" "app_gw_app" {
+  name                = local.app_service_subdomain
   zone_name           = data.azurerm_dns_zone.dns.name
   resource_group_name = data.azurerm_dns_zone.dns.resource_group_name
   ttl                 = 300
-  target_resource_id  = azurerm_public_ip.appgw.id
+  record              = azurerm_app_service.app["auth"].default_site_hostname
 }
 
+# TXT record for verifying domain ownership
 resource "azurerm_dns_txt_record" "app" {
-  name                = "asuid.${azurerm_dns_a_record.app_gw_app.name}"
+  name                = "asuid.${local.app_service_subdomain}"
   zone_name           = data.azurerm_dns_zone.dns.name
   resource_group_name = data.azurerm_dns_zone.dns.resource_group_name
   ttl                 = 300
@@ -20,4 +36,13 @@ resource "azurerm_dns_txt_record" "app" {
   record {
     value = azurerm_app_service.app["auth"].custom_domain_verification_id
   }
+}
+
+# DNS records for static storage
+resource "azurerm_dns_a_record" "app_gw_static" {
+  name                = "appgw-static"
+  zone_name           = data.azurerm_dns_zone.dns.name
+  resource_group_name = data.azurerm_dns_zone.dns.resource_group_name
+  ttl                 = 300
+  target_resource_id  = azurerm_public_ip.appgw.id
 }
