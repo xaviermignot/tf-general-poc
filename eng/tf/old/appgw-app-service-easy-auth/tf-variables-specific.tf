74facabd58b43a0d36068eb04f2536fe7549926f
Xavier Mignot <code@xmi.fr>
2022-01-06 11:24:22 +0100

    Back to CDN

---
+++ eng/tf/old/appgw-app-service-easy-auth/tf-variables-specific.tf
@@ -0,0 +1,44 @@
+variable "custom_domain_name" {
+  type        = string
+  description = "the custom domain name for app gateway"
+}
+
+variable "storage_custom_domain" {
+  type        = string
+  description = "the custom domain for static http storage"
+}
+
+variable "app_service_custom_domain" {
+  type        = string
+  description = "the custom domain for app service"
+}
+
+variable "organization_name" {
+  type        = string
+  description = "the name of the organization for the ssl cert"
+}
+
+variable "whitelisted_ips" {
+  type        = set(string)
+  description = "list of IPs to whitelist on various resources"
+}
+
+locals {
+  app_services = {
+    "auth" = {
+      name             = "web-${var.project}-auth"
+      easy_auth        = true
+      custom_subdomain = "appgw-app-auth"
+    }
+    "no-auth" = {
+      name             = "web-${var.project}-no-auth"
+      easy_auth        = false
+      custom_subdomain = "appgw-app-no-auth"
+    }
+    "new" = {
+      name             = "web-${var.project}-new"
+      easy_auth        = false
+      custom_subdomain = "appgw-app-new"
+    }
+  }
+}
