332bd0c04b540f3ef96caeb2502dcffa33c4c746
Xavier Mignot <code@xmi.fr>
2023-09-22 17:12:26 -0400

    feat: Put appgw back

--- eng/tf/tf-main.tf
+++ eng/tf/tf-main.tf
@@ -48,58 +48,45 @@ module "acme" {
   dns_zone_rg_name = var.dns_config.zone_rg_name
 }
 
-# locals {
-#   app_services = {
-#     "auth" = {
-#       name              = "web-${var.project}-auth"
-#       easy_auth         = true
-#       custom_subdomain  = "appgw-app-auth"
-#       use_custom_domain = true
-#     }
-#     "no-auth" = {
-#       name              = "web-${var.project}-no-auth"
-#       easy_auth         = false
-#       custom_subdomain  = "appgw-app-no-auth"
-#       use_custom_domain = true
-#     }
-#     "new" = {
-#       name              = "web-${var.project}-new"
-#       easy_auth         = false
-#       custom_subdomain  = "appgw-app-new"
-#       use_custom_domain = true
-#     }
-#     "auth-custom" = {
-#       name              = "web-${var.project}-auth-custom"
-#       easy_auth         = true
-#       custom_subdomain  = "appgw-app-auth-custom"
-#       use_custom_domain = true
-#     }
-#   }
-# }
+locals {
+  app_services = {
+    "auth" = {
+      name              = "web-${var.project}-auth"
+      easy_auth         = true
+      custom_subdomain  = "appgw-app-auth"
+      use_custom_domain = true
+    }
+    "no-auth" = {
+      name              = "web-${var.project}-no-auth"
+      easy_auth         = false
+      custom_subdomain  = "appgw-app-no-auth"
+      use_custom_domain = true
+    }
+    "new" = {
+      name              = "web-${var.project}-new"
+      easy_auth         = false
+      custom_subdomain  = "appgw-app-new"
+      use_custom_domain = true
+    }
+    "auth-custom" = {
+      name              = "web-${var.project}-auth-custom"
+      easy_auth         = true
+      custom_subdomain  = "appgw-app-auth-custom"
+      use_custom_domain = true
+    }
+  }
+}
 
-# module "appgw-app-service-easy-auth" {
-#   source = "./appgw-app-service-easy-auth"
-
-#   rg_name  = azurerm_resource_group.rg.name
-#   location = var.location
-#   project  = var.project
-
-#   app_services        = local.app_services
-#   dns_zone_name       = var.dns_config.zone_name
-#   dns_zone_rg_name    = var.dns_config.zone_rg_name
-
-#   wildcard_cert = {
-#     pfx_value    = module.acme.pfx_value
-#     pfx_password = module.acme.pfx_password
-#   }
-# }
-
-module "app_service_cert" {
-  source = "./app_service_cert"
+module "appgw-app-service-easy-auth" {
+  source = "./appgw-app-service-easy-auth"
 
   rg_name  = azurerm_resource_group.rg.name
   location = var.location
   project  = var.project
+
+  app_services        = local.app_services
+  dns_zone_name       = var.dns_config.zone_name
+  dns_zone_rg_name    = var.dns_config.zone_rg_name
 
   wildcard_cert = {
     pfx_value    = module.acme.pfx_value
@@ -107,66 +94,79 @@ module "app_service_cert" {
   }
 }
 
-module "app_service_plan" {
-  source = "./app_service_plan"
+# module "app_service_cert" {
+#   source = "./app_service_cert"
 
-  rg_name  = azurerm_resource_group.plan.name
-  location = var.location
-  project  = var.project
-}
+#   rg_name  = azurerm_resource_group.rg.name
+#   location = var.location
+#   project  = var.project
 
-module "app_service_docker" {
-  source = "./app_service"
+#   wildcard_cert = {
+#     pfx_value    = module.acme.pfx_value
+#     pfx_password = module.acme.pfx_password
+#   }
+# }
 
-  rg_name  = azurerm_resource_group.rg.name
-  location = var.location
-  project  = var.project
+# module "app_service_plan" {
+#   source = "./app_service_plan"
 
-  name = "docker"
+#   rg_name  = azurerm_resource_group.plan.name
+#   location = var.location
+#   project  = var.project
+# }
 
-  blue_app = {
-    type    = "docker"
-    version = "xaviermignot/tfgeneralpoc"
-    tag     = "host"
-  }
+# module "app_service_docker" {
+#   source = "./app_service"
 
-  green_app = {
-    type    = "docker"
-    version = "xaviermignot/tfgeneralpoc"
-    tag     = "hello"
-  }
+#   rg_name  = azurerm_resource_group.rg.name
+#   location = var.location
+#   project  = var.project
 
-  active_app = "blue"
+#   name = "docker"
 
-  dns_zone_name              = var.dns_config.zone_name
-  dns_zone_rg_name           = var.dns_config.zone_rg_name
-  app_service_plan_id        = module.app_service_plan.app_service_plan_id
-  app_service_certificate_id = module.app_service_cert.app_service_certificate_id
-}
+#   blue_app = {
+#     type    = "docker"
+#     version = "xaviermignot/tfgeneralpoc"
+#     tag     = "host"
+#   }
 
-module "app_service_package" {
-  source = "./app_service"
+#   green_app = {
+#     type    = "docker"
+#     version = "xaviermignot/tfgeneralpoc"
+#     tag     = "hello"
+#   }
 
-  rg_name  = azurerm_resource_group.rg.name
-  location = var.location
-  project  = var.project
+#   active_app = "blue"
 
-  name = "package"
+#   dns_zone_name              = var.dns_config.zone_name
+#   dns_zone_rg_name           = var.dns_config.zone_rg_name
+#   app_service_plan_id        = module.app_service_plan.app_service_plan_id
+#   app_service_certificate_id = module.app_service_cert.app_service_certificate_id
+# }
 
-  blue_app = {
-    type    = "dotnet"
-    version = "5.0"
-  }
+# module "app_service_package" {
+#   source = "./app_service"
 
-  green_app = {
-    type    = "dotnet"
-    version = "6.0"
-  }
+#   rg_name  = azurerm_resource_group.rg.name
+#   location = var.location
+#   project  = var.project
 
-  active_app = "blue"
+#   name = "package"
 
-  dns_zone_name              = var.dns_config.zone_name
-  dns_zone_rg_name           = var.dns_config.zone_rg_name
-  app_service_plan_id        = module.app_service_plan.app_service_plan_id
-  app_service_certificate_id = module.app_service_cert.app_service_certificate_id
-}
+#   blue_app = {
+#     type    = "dotnet"
+#     version = "5.0"
+#   }
+
+#   green_app = {
+#     type    = "dotnet"
+#     version = "6.0"
+#   }
+
+#   active_app = "blue"
+
+#   dns_zone_name              = var.dns_config.zone_name
+#   dns_zone_rg_name           = var.dns_config.zone_rg_name
+#   app_service_plan_id        = module.app_service_plan.app_service_plan_id
+#   app_service_certificate_id = module.app_service_cert.app_service_certificate_id
+# }
