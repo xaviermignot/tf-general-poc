309f76a5fb4e2dda6774569b9e477bd0ecd3e49c
Xavier Mignot <code@xmi.fr>
2024-05-01 09:07:08 -0400

    feat: Remove plan rg

--- eng/tf/az-rg.tf
+++ eng/tf/az-rg.tf
@@ -2,8 +2,3 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-${var.project}"
   location = var.location
 }
-
-resource "azurerm_resource_group" "plan" {
-  name     = "rg-${var.project}-plan"
-  location = var.location
-}
